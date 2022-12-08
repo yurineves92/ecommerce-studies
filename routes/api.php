13ed779b87aa395456923d09ac0f8aaa638674ef
Yuri Neves <yurineves92@gmail.com>
2022-12-07 23:36:47 -0300

    studying.... products routes and requests

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -18,6 +19,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware(['auth:sanctum', 'admin'])->group(function () {
     Route::get('/user', [AuthController::class, 'getUser'])->name('user');
     Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+    /** Products Routes */
+    Route::apiResource('products', ProductController::class);
 });
 
 
